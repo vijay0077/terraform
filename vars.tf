f9b924ae5de1dfc209a9e8874d9504b6d6d10bad
hisrarul <35391097+hisrarul@users.noreply.github.com>
2019-09-23 12:11:35 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "AMI" {
+    default = "ami-0b69ea66ff7391e80"
+}
+
+variable "INST_TYPE" {
+  default = "t2.micro"
+}
+
+variable "SUBNET" {
+  default = "subnet-0b43102c95508605d"
+}
+
+variable "TAG" {
+  default = "Terraform-demo-var"
+}
+
+variable "AMI2" {
+  default = "ami-02eac2c0129f6376b"
+}
+
