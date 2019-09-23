982002c4c850b0deadfdf076dc6d6ca2da42cbf1
hisrarul <35391097+hisrarul@users.noreply.github.com>
2019-09-23 12:10:49 +0530

    create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "webserver" {
+  ami = "${var.AMI}"
+  instance_type = "${var.INST_TYPE}"
+  subnet_id = "${var.SUBNET}"
+  tags = {
+    Name = "${var.TAG}"
+  }
+}
+
+resource "aws_instance" "webserver2" {
+  ami = "${var.AMI2}"
+  instance_type = "${var.INST_TYPE}"
+  subnet_id = "${var.SUBNET}"
+  tags = {
+    Name = "${var.TAG}"
+  }
+}
